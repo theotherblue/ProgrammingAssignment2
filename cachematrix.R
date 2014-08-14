80c1305c24ac166811b4e5c2718a673322aac62e
Thom Black <theotherblue@gmail.com>
2014-08-14 22:15:39 +0200

    Implemented functions for caching an inverted matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+##
+## Two functions that makes it easy to implement a chaching scheeme 
+## in your project
+##
+## Usage:
+## m <- matrix(4:9, 2,2)
+## mc <- makeCacheMatrix(m)
+## cacheSolve(mc)
+##
 
-## Write a short comment describing this function
+## Wraps a matrix in an chaching environment containing
+## getters and setters.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) inv <<- inverse
+    getinverse <- function() inv
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Inverts a matrix. 
+## Checks the chache before inverting the matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinverse()
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data, ...)
+    x$setinverse(inv)
+    inv
 }
